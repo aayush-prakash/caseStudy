2b5d26078a2c2270297624722da05d0925ea7480
aayush-prakash <aayushtyson@gmail.com>
2021-04-27 18:59:56 +0530

    Update StudentController.java

--- LearnToday-RESTAPI/src/main/java/com/cognizant/controller/StudentController.java
+++ LearnToday-RESTAPI/src/main/java/com/cognizant/controller/StudentController.java
@@ -15,34 +15,37 @@ import org.springframework.web.bind.annotation.RestController;
 import com.cognizant.dao.StudentDao;
 import com.cognizant.model.Course;
 import com.cognizant.model.Student;
+import com.cognizant.service.StudentService;
 
 @RestController
 public class StudentController {
-	@Autowired
-	StudentDao studentDao;
+    @Autowired
+    StudentService studentService;
+    @Autowired
+    StudentDao studentDao;
 
-	@GetMapping("/student")
-	public ResponseEntity<Object> getAllCourses() {
-		List<Course> courseList = studentDao.getAllCourses();
-		if(courseList.isEmpty()) {
-			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(courseList);
-		}
-		return ResponseEntity.status(HttpStatus.OK).body(courseList);
-	}
-	
-	@PostMapping("/student")	
-	public ResponseEntity<Object> postStudent(@RequestBody Student student){
-		if(studentDao.postStudent(student) == 1) {
-			return ResponseEntity.status(HttpStatus.CREATED).body(student);
-		}
-		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(student);
-	}
-	
-	@DeleteMapping("/student/{id}")
-	public ResponseEntity<Object> deleteStudentEnrollment(@PathVariable int id){
-		if(studentDao.deleteById(id)==1) {
-			return ResponseEntity.status(HttpStatus.OK).body("");
-		}
-		return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Searched Data not Found");
-	}
+    @GetMapping("/student")
+    public ResponseEntity<Object> getAllCourses() {
+        List<Course> courseList = studentService.getAllCourses();
+        if(courseList.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(courseList);
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(courseList);
+    }
+    
+    @PostMapping("/student")    
+    public ResponseEntity<Object> postStudent(@RequestBody Student student){
+        if(studentService.postStudent(student)) {
+            return ResponseEntity.status(HttpStatus.CREATED).body(student);
+        }
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(student);
+    }
+    
+    @DeleteMapping("/student/{id}")
+    public ResponseEntity<Object> deleteStudentEnrollment(@PathVariable int id){
+        if(studentService.deleteById(id)) {
+            return ResponseEntity.status(HttpStatus.OK).body("");
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Searched Data not Found");
+    }
 }
